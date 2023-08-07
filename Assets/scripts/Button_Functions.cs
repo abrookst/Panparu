bc8fba722c5f351510b7b8742e40adf4f09a2451
ChickoonLord <67387813+DavidColetta@users.noreply.github.com>
2023-08-07 11:50:24 -0400

    Feed animation

--- Assets/scripts/Button_Functions.cs
+++ Assets/scripts/Button_Functions.cs
@@ -1,11 +1,20 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Button_Functions : MonoBehaviour
 {
 
-    public GameObject clock;
+    [SerializeField] private GameObject clock;
+
+    //Get button references
+    [SerializeField] private Button FeedButton;
+    [SerializeField] private Button PetButton;
+    [SerializeField] private Button PlayButton;
+    [SerializeField] private Button TimeButton;
+    
 
     // Start is called before the first frame update
     void Start()
@@ -17,5 +26,33 @@ public class Button_Functions : MonoBehaviour
     public void ToggleTime()
     {
         clock.SetActive(!clock.activeInHierarchy);
+    }
+
+    public void Feed() {
+        StartCoroutine(FeedCoroutine());
+    }
+    IEnumerator FeedCoroutine() {
+        FeedButton.interactable = false;
+        PetButton.interactable = false;
+        PlayButton.interactable = false;
+        TimeButton.interactable = false;
+
+        //Create blank UI image
+        GameObject image = new GameObject("Food", typeof(RectTransform), typeof(Image));
+        image.transform.SetParent(GameObject.Find("Canvas").transform);
+        
+        //Play rotate animation on Panparu
+        Panparu.Instance.GetComponent<Animator>().SetTrigger("Feed");
+        yield return new WaitForSeconds(2);
+
+        Panparu.Instance.Feed();
+
+        Destroy(image);
+
+        FeedButton.interactable = true;
+        PetButton.interactable = true;
+        PlayButton.interactable = true;
+        TimeButton.interactable = true;
+        
     }
 }
--- Assets/scripts/Panparu.cs
+++ Assets/scripts/Panparu.cs
@@ -10,21 +10,25 @@ public class Panparu : MonoBehaviour
 
     private DateTime birthTime;
     private DateTime lastTimeHungry;
+    public static Panparu Instance { get; private set; }
 
     void Start()
     {
+        Instance = this;
         birthTime = DateTime.Now;
         lastTimeHungry = DateTime.Now;
     }
     void Update()
     {
+        TimeSpan hungerCooldown = new(0, 0, 6);
         DateTime timeNow = DateTime.Now;
         //Get difference between lastTimeHungry and timeNow
         TimeSpan timeSinceHungry = timeNow - lastTimeHungry;
+        //Get duration of timeSinceHungry in seconds
         //If timeSinceHungry is greater than 6 seconds, reduce food by 1
-        if (timeSinceHungry.Seconds > 6) {
+        if (timeSinceHungry.CompareTo(hungerCooldown) > 0) {
             food -= 1;
-            lastTimeHungry = timeNow;
+            lastTimeHungry = lastTimeHungry.Add(hungerCooldown);
         }
     }
 
