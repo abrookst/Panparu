3ddcbbc5fbd40b3fd3e97275381421ff84c20592
ChickoonLord <67387813+DavidColetta@users.noreply.github.com>
2023-08-07 12:25:17 -0400

    attention and play decrease with time

--- Assets/scripts/Panparu.cs
+++ Assets/scripts/Panparu.cs
@@ -10,13 +10,20 @@ public class Panparu : MonoBehaviour
 
     private DateTime birthTime;
     private DateTime lastTimeHungry;
+    private DateTime lastTimeAttention;
+    private DateTime lastTimePlay;
     public static Panparu Instance { get; private set; }
+
+    TimeSpan attentionCooldown = new(0, 0, 12);
+    TimeSpan playCooldown = new(0, 0, 12);
 
     void Start()
     {
         Instance = this;
         birthTime = DateTime.Now;
         lastTimeHungry = DateTime.Now;
+        lastTimeAttention = DateTime.Now;
+        lastTimePlay = DateTime.Now;
     }
     void Update()
     {
@@ -25,33 +32,52 @@ public class Panparu : MonoBehaviour
         //Get difference between lastTimeHungry and timeNow
         TimeSpan timeSinceHungry = timeNow - lastTimeHungry;
         //If timeSinceHungry is greater than 6 seconds, reduce food by 1
-        //Repeat until timeSinceHungry is less than 6 seconds
         while (timeSinceHungry.CompareTo(hungerCooldown) > 0) {
             food -= 1;
             lastTimeHungry = lastTimeHungry.Add(hungerCooldown);
             timeSinceHungry = timeNow - lastTimeHungry;
         }
-        print(food);
+
+        TimeSpan timeSinceAttention = timeNow - lastTimeAttention;
+        if (timeSinceAttention.CompareTo(attentionCooldown) > 0)
+        {
+            if (attention > 0)
+                attention -= 1;
+            lastTimeAttention = lastTimeAttention.Add(attentionCooldown);
+        }
+
+        TimeSpan timeSincePlay = timeNow - lastTimePlay;
+        if (timeSincePlay.CompareTo(playCooldown) > 0)
+        {
+            if (play > 0)
+                play -= 1;
+            lastTimePlay = lastTimePlay.Add(playCooldown);
+        }
     }
 
     public void Feed()
     {
-        if (food < 4)
+        if (food < 4) {
             food += 1;
-        else
+            lastTimeHungry = DateTime.Now;
+        } else
             Debug.Log("I'm full!");
     }
     public void Pet()
     {
-        if (attention < 1)
+        if (attention < 1) {
             attention += 1;
+            lastTimeAttention = DateTime.Now;
+        }
         else
             Debug.Log("I'm already happy!");
     }
     public void Play()
     {
-        if (play < 1)
+        if (play < 1) {
             play += 1;
+            lastTimePlay = DateTime.Now;
+        }
         else
             Debug.Log("I'm tired!");
     }
