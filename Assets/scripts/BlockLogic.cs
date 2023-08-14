ac4e31ada7f6142c34a5992ac7e8063d8fb5ac2a
ChickoonLord <67387813+DavidColetta@users.noreply.github.com>
2023-08-14 15:42:36 -0400

    Swipe down

--- Assets/scripts/BlockLogic.cs
+++ Assets/scripts/BlockLogic.cs
@@ -13,7 +13,15 @@ public class BlockLogic : MonoBehaviour
     bool moveable = true;
     public RectTransform rig;
     BlockLogic ghost;
-    // Start is called before the first frame update
+    
+
+    //Swipe detection
+    private Vector2 fingerDown;
+    private Vector2 fingerUp;
+    public bool detectSwipeOnlyAfterRelease = false;
+    public float SWIPE_THRESHOLD = 20f;
+
+    public float swipeDown = 0;
     void Start()
     {
         GameObject canvasObj = GameObject.Find("Canvas");
@@ -101,6 +109,33 @@ public class BlockLogic : MonoBehaviour
             {
                 MinigameManager.curMode = MinigameManager.ControlMode.keyboard;
             }
+        } else if (MinigameManager.curMode == MinigameManager.ControlMode.mobile) {
+            swipeDown -= Time.deltaTime;
+            foreach (Touch touch in Input.touches)
+            {
+                if (touch.phase == TouchPhase.Began)
+                {
+                    fingerUp = touch.position;
+                    fingerDown = touch.position;
+                }
+
+                //Detects Swipe while finger is still moving
+                if (touch.phase == TouchPhase.Moved)
+                {
+                    if (!detectSwipeOnlyAfterRelease)
+                    {
+                        fingerDown = touch.position;
+                        checkSwipe();
+                    }
+                }
+
+                //Detects swipe after finger is released
+                if (touch.phase == TouchPhase.Ended)
+                {
+                    fingerDown = touch.position;
+                    checkSwipe();
+                }
+            }
         }
         
 
@@ -116,16 +151,14 @@ public class BlockLogic : MonoBehaviour
             } 
             else if(MinigameManager.curMode == MinigameManager.ControlMode.mobile)
             {
-                try {
+                if (Input.touchCount > 0 && swipeDown <= 0) {
                     Touch touch = Input.GetTouch(0);
                     var touchPos = touch.position;
                     left = touchPos.x < (Screen.width / 2);
                     right = touchPos.x > (Screen.width / 2);
-                } catch (System.Exception e) {
-                    Debug.Log(e);
                 }
             }
-            else {
+            else if (MinigameManager.curMode == MinigameManager.ControlMode.keyboard){
                 left = Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow);
                 right = Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow);
             }
@@ -145,13 +178,25 @@ public class BlockLogic : MonoBehaviour
         
         //Harddrop
         if (MinigameManager.curMode == MinigameManager.ControlMode.mobile) {
-            if (dropTimer > MinigameManager.dropTime) {
-                gameObject.transform.localPosition += new Vector3(0, -2, 0);
-                if (!CheckValid()) {
-                    gameObject.transform.localPosition += new Vector3(0, 2, 0);
-                    BlockLanded();
+            bool softDrop = swipeDown > 0;
+            if (softDrop) {
+                if (fallTimer > MinigameManager.fallTime) {
+                    gameObject.transform.localPosition += new Vector3(0, -2, 0);
+                    if (!CheckValid()) {
+                        gameObject.transform.localPosition += new Vector3(0, 2, 0);
+                        BlockLanded();
+                    }
+                    fallTimer = 0;
                 }
-                dropTimer = 0;
+            } else {
+                    if (dropTimer > MinigameManager.dropTime) {
+                    gameObject.transform.localPosition += new Vector3(0, -2, 0);
+                    if (!CheckValid()) {
+                        gameObject.transform.localPosition += new Vector3(0, 2, 0);
+                        BlockLanded();
+                    }
+                    dropTimer = 0;
+                }
             }
         } else {
             if (Input.GetMouseButton(0) || Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S)) {
@@ -215,4 +260,74 @@ public class BlockLogic : MonoBehaviour
         Destroy(gameObject);
     }
 
+    void checkSwipe()
+    {
+        //Check if Vertical swipe
+        if (verticalMove() > SWIPE_THRESHOLD && verticalMove() > horizontalValMove())
+        {
+            //Debug.Log("Vertical");
+            if (fingerDown.y - fingerUp.y > 0)//up swipe
+            {
+                OnSwipeUp();
+            }
+            else if (fingerDown.y - fingerUp.y < 0)//Down swipe
+            {
+                OnSwipeDown();
+            }
+            fingerUp = fingerDown;
+        }
+
+        //Check if Horizontal swipe
+        else if (horizontalValMove() > SWIPE_THRESHOLD && horizontalValMove() > verticalMove())
+        {
+            //Debug.Log("Horizontal");
+            if (fingerDown.x - fingerUp.x > 0)//Right swipe
+            {
+                OnSwipeRight();
+            }
+            else if (fingerDown.x - fingerUp.x < 0)//Left swipe
+            {
+                OnSwipeLeft();
+            }
+            fingerUp = fingerDown;
+        }
+
+        //No Movement at-all
+        else
+        {
+            //Debug.Log("No Swipe!");
+        }
+    }
+
+    float verticalMove()
+    {
+        return Mathf.Abs(fingerDown.y - fingerUp.y);
+    }
+
+    float horizontalValMove()
+    {
+        return Mathf.Abs(fingerDown.x - fingerUp.x);
+    }
+
+    //////////////////////////////////CALLBACK FUNCTIONS/////////////////////////////
+    void OnSwipeUp()
+    {
+        Debug.Log("Swipe UP");
+    }
+
+    void OnSwipeDown()
+    {
+        Debug.Log("Swipe Down");
+        swipeDown = Time.deltaTime * 3;
+    }
+
+    void OnSwipeLeft()
+    {
+        Debug.Log("Swipe Left");
+    }
+
+    void OnSwipeRight()
+    {
+        Debug.Log("Swipe Right");
+    }
 }
